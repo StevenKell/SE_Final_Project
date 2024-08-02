f3a09fa3873cad1c20fe6b94c9bd563d490b08ae
StevenKell <steventkell@gmail.com>
2024-08-02 09:57:18 -0400

    Added submit score button, comments to help naviagate code and where to put in new code.

--- static/scripts.js
+++ static/scripts.js
@@ -1,91 +1,112 @@
-let rondomNum=Math.trunc(Math.random()*100)+1;
+let rondomNum = Math.trunc(Math.random() * 100) + 1;
 console.log(rondomNum);
 
-let currentScore=10;
-let historyHighScore=0;
+let currentScore = 10;
+let historyHighScore = 0;
 let guesshistory = [];
-let checkbtn=document.querySelector('.checkbtn')
-let reset=document.querySelector('.resetbtn');
-let msg=document.querySelector('p.statusText')
-let userInput=document.querySelector('input');
-let currntScore=document.querySelector('.crrntscore');
-let resetbtn=document.querySelector('.resetbtn')
-let historyHighS=document.querySelector('.highscore')
 let guesscompare = 0;
 
-resetbtn.addEventListener('click',function()
-{
-currentScore=10;
-historyHighScore=0;
-currntScore.textContent=String(currentScore);
-historyHighS.textContent=String(historyHighScore);
-msg.textContent='Guess a number';
-        msg.style.color='black';
-        msg.style.fontSize='medium';
-        document.getElementById("questionmark").src = "images/894-8949235_noun-project-question-mark-bubble-icon-1328413-cc.png";
-        userInput.style.display = "inline";
-        checkbtn.style.display = "inline";
-        guesshistory = [];
-        
+let checkbtn = document.querySelector('.checkbtn')
+let msg = document.querySelector('p.statusText')
+let userInput = document.querySelector('input');
+let currntScore = document.querySelector('.crrntscore');
+let resetbtn = document.querySelector('.resetbtn')
+let submitScoreBTN = document.querySelector('.submitScoreBTN')
+let historyHighS = document.querySelector('.highscore')
+/////////////////////////////////////
+/////Add selector for input box here.
+////////////////////////////////////
+
+
+//////////////////
+/* Reset Button */
+/////////////////
+resetbtn.addEventListener('click', function () {
+    currentScore = 10;
+    historyHighScore = 0;
+    currntScore.textContent = String(currentScore);
+    historyHighS.textContent = String(historyHighScore);
+    msg.textContent = 'Guess a number';
+    msg.style.color = 'black';
+    msg.style.fontSize = 'medium';
+    document.getElementById("questionmark").src = "static/images/894-8949235_noun-project-question-mark-bubble-icon-1328413-cc.png";
+    userInput.style.display = "inline";
+    checkbtn.style.display = "inline";
+    guesshistory = [];
+
 }
 )
+///////////////////////////////////////////////////
+/* Button to see if guess matches random number. */
+///////////////////////////////////////////////////
+checkbtn.addEventListener('click', function () {
 
-
-checkbtn.addEventListener('click',function()
-{
-
-    guesshistory.push(userInput.value); 
+    guesshistory.push(userInput.value);
     console.log(guesshistory);
-if(currentScore>0){
-    if(Number(userInput.value)===rondomNum){
-        msg.textContent='Congrats! you win the game!';
-        msg.style.color='green';
-        msg.style.fontSize='medium';
-        document.getElementById("questionmark").src = "images/you-win-neon-signs-style-text-vector.jpg";
-        userInput.style.display = "none";
-        checkbtn.style.display = "none";
-    if (currentScore>historyHighScore){
-        historyHighScore=currentScore;
-        historyHighS.textContent=String(historyHighScore);
-        }
-    }
-    else{
-       
-        if(Number(userInput.value)>rondomNum){
-            msg.textContent='Wrong number! too high! try smaller value';
-            msg.style.color='yellow';
-            msg.style.fontSize='small';
-            currentScore=currentScore-1;
-            currntScore.textContent=String(currentScore);
-            guesscompare = userInput;
-            guesshistory.push(userInput.value);
-            console.log(guesshistory)
-        }
-        if(Number(userInput.value)<rondomNum){
-            msg.textContent='Wrong number! too low! try a larger value';
-            msg.style.color='yellow';
-            msg.style.fontSize='small';
-            currentScore=currentScore-1;
-            currntScore.textContent=String(currentScore);
-              
-
-        }
-        if(currentScore===0){
-            msg.textContent='You lose! Play again!';
-            msg.style.color='red';
-            msg.style.fontSize='medium';
+    if (currentScore > 0) {
+        ///////////////////
+        /* Win condition */
+        //////////////////
+        if (Number(userInput.value) === rondomNum) {
+            msg.textContent = `Congrats! you win the game! \n
+            Now hit the button to submit it to the score board!`;
+            msg.style.color = 'green';
+            msg.style.fontSize = 'medium';
+            document.getElementById("questionmark").src = "static/images/you-win-neon-signs-style-text-vector.jpg";
             userInput.style.display = "none";
             checkbtn.style.display = "none";
-            document.getElementById("questionmark").src = "images/OIP.jpeg";
-
-            console.log("lose condition fired");
+            submitScoreBTN.style.display = "block";
+            if (currentScore > historyHighScore) {
+                historyHighScore = currentScore;
+                historyHighS.textContent = String(historyHighScore);
+            }
         }
+        else {
+            //////////////////
+            //Guess too high//
+            /////////////////
+            if (Number(userInput.value) > rondomNum) {
+                msg.textContent = 'Wrong number! too high! try smaller value';
+                msg.style.color = 'yellow';
+                msg.style.fontSize = 'small';
+                currentScore = currentScore - 1;
+                currntScore.textContent = String(currentScore);
+                guesscompare = userInput;
+                guesshistory.push(userInput.value);
+                console.log(guesshistory)
+            }
+            //////////////////
+            /* Guess too low */
+            //////////////////
+            if (Number(userInput.value) < rondomNum) {
+                msg.textContent = 'Wrong number! too low! try a larger value';
+                msg.style.color = 'yellow';
+                msg.style.fontSize = 'small';
+                currentScore = currentScore - 1;
+                currntScore.textContent = String(currentScore);
+
+
+            }
+            //////////////////////
+            /*  Lose condition  */
+            //////////////////////
+            if (currentScore === 0) {
+                msg.textContent = 'You lose! Play again!';
+                msg.style.color = 'red';
+                msg.style.fontSize = 'medium';
+                userInput.style.display = "none";
+                checkbtn.style.display = "none";
+                document.getElementById("questionmark").src = "static/images/OIP.jpeg";
+
+                console.log("lose condition fired");
+            }
+        }
+
     }
 
 }
 
-}
-
 )
-
-
+/////////////////////////////////////////
+// Add event handler for submit score button
+///////////////////////////////////////
